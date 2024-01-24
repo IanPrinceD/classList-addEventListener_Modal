c48d5526f616ad672ac8982f2a3fe1c1bbec3b89
IanPrinceD <princedumanhug@gmail.com>
2024-01-24 13:44:15 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.close-modal');
+const btns = document.querySelectorAll('.show-modal');
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener('click', openModal);
+}
+
+closeBtn.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
