2e54882d86878d23a8546cee59c04c5dcac4eb4b
IanPrinceD <princedumanhug@gmail.com>
2024-01-24 20:00:35 +0800

    added addEventListener keydown event

--- script.js
+++ script.js
@@ -22,3 +22,9 @@ for (let i = 0; i < btns.length; i++) {
 closeBtn.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
